0850986b855574928198f2fd569940a1a797ff88
RyuSw-cs <rsw1452@naver.com>
2024-08-28 01:33:00 +0900

    fixed navigate bug

--- feature/filter/src/main/java/com/cmc/purithm/feature/filter/ui/FilterLoadingFragment.kt
+++ feature/filter/src/main/java/com/cmc/purithm/feature/filter/ui/FilterLoadingFragment.kt
@@ -2,8 +2,10 @@ package com.cmc.purithm.feature.filter.ui
 
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.addCallback
 import androidx.navigation.fragment.navArgs
 import com.cmc.purithm.common.base.BaseFragment
+import com.cmc.purithm.common.dialog.CommonDialogFragment
 import com.cmc.purithm.feature.filter.R
 import com.cmc.purithm.feature.filter.databinding.FragmentFilterLoadingBinding
 
@@ -20,9 +22,14 @@ class FilterLoadingFragment : BaseFragment<FragmentFilterLoadingBinding>() {
     }
 
     override fun initView() {
+        setBackButtonEvent()
         Handler(Looper.getMainLooper()).postDelayed({
             navigate(FilterLoadingFragmentDirections.actionFilterLoadingFragmentToFilterValueFragment(navArgs.filterId))
         }, DELAY_TIME)
+    }
+
+    private fun setBackButtonEvent(){
+        requireActivity().onBackPressedDispatcher.addCallback(this) {}
     }
 
     companion object {
